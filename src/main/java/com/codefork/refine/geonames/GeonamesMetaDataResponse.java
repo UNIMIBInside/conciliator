9f5d3a8cb42ebec2087dba107e38bf27b39e7b88
Unknown <v.cutrona1@gmail.com>
2018-08-06 17:04:08 +0200

    Update Geonames service metadata with the extend parameter

--- src/main/java/com/codefork/refine/geonames/GeonamesMetaDataResponse.java
+++ src/main/java/com/codefork/refine/geonames/GeonamesMetaDataResponse.java
@@ -1,8 +1,6 @@
 package com.codefork.refine.geonames;
 
-import com.codefork.refine.resources.NameType;
-import com.codefork.refine.resources.ServiceMetaDataResponse;
-import com.codefork.refine.resources.View;
+import com.codefork.refine.resources.*;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.ArrayList;
@@ -15,6 +13,7 @@ public class GeonamesMetaDataResponse extends ServiceMetaDataResponse {
     private final static View VIEW = new View("http://sws.geonames.org/{{id}}");
     private final static String SCHEMA_SPACE = "http://www.geonames.org/ontology#";
     private final static List<NameType> DEFAULT_TYPES = new ArrayList<>();
+    private final static Extend EXTEND = new Extend(null);
 
     static {
         DEFAULT_TYPES.add(new NameType("Feature", "Feature"));
@@ -26,6 +25,7 @@ public class GeonamesMetaDataResponse extends ServiceMetaDataResponse {
         setSchemaSpace(SCHEMA_SPACE);
         setView(VIEW);
         setDefaultTypes(DEFAULT_TYPES);
+        setExtend(EXTEND);
     }
 
 }
